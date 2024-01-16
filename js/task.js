19841c106fbf2b1301b1d363ef9b86351763f939
leila <leylag659@gmail.com>
2024-01-16 16:51:16 +0400

    added task.js and class task

---
+++ js/task.js
@@ -0,0 +1,52 @@
+class Task {
+    #id;
+    #title;
+    #description;
+    #date;
+    #isCompleted;
+  
+    constructor(name, _description) {
+      this.#id = crypto.randomUUID();
+      this.#title = name;
+      this.#description = _description;
+      this.#date = new Date();
+      this.#isCompleted = false;
+    }
+  
+    get task() {
+      return {
+        id: this.id,
+        title: this.title_get,
+        description: this.description_get,
+        date: this.date_get,
+        isCompleted: this.isCompleted_get,
+      };
+    }
+  
+    get id() {
+      return this.#id;
+    }
+  
+    get title_get() {
+      return this.#title;
+    }
+  
+    get description_get() {
+      return this.#description;
+    }
+  
+    get date_get() {
+      return this.#date;
+    }
+  
+    get isCompleted_get() {
+      return this.#isCompleted;
+    }
+  
+    toggle_completion_status() {
+      this.#isCompleted = !this.#isCompleted;
+    }
+  }
+  
+
+  
